eedbfb1b023cb2de1154e23ec6cecaddd110d465
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 18:39:42 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,8 +4,8 @@ def consolidate_cart(cart)
   cart.each do |element|
     element.each do |item, info|
         consolidated_hash[item] = info
-        info[:count] ||=0
-        info[:count] +=1
+        item[info][:count] ||=0
+        item[info][:count] +=1
     end
   end
 
