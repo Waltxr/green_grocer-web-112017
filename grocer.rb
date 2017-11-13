1de8f80258a03fe1a84168df53f3b33425c75fff
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 17:15:12 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -17,9 +17,9 @@ def apply_coupons(cart, coupons)
     cart["#{coupon[:item]} W/COUPON"] = {
       :price=>coupon[:cost],
       :clearance=>cart[coupon[:item]][:clearance],
-      :count => 0
     }
-    cart["#{coupon[:item]} W/COUPON"][:count] += 1
+    cart["#{coupon[:item]} W/COUPON"][:count] ||= 0
+    cart["#{coupon[:item]} W/COUPON"][:count] += 0
     cart[coupon[:item]][:count] -= coupon[:num]
   end
 
