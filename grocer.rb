409ebccc1a1c9db3d047f2bea58330d21346b172
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 17:51:23 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
         :clearance=>cart[coupon[:item]][:clearance],
         :count => 1
       }
-    end 
+    end
     cart[coupon[:item]][:count] -= coupon[:num]
   end
 
