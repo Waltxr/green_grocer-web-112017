d370259463e1ba140f6d036fd70af5bc6bf35b01
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 18:07:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  coupons_applied = apply_coupons(consolidated_cart)
+  coupons_applied = apply_coupons(consolidated_cart, coupons)
   discounts_applied = apply_clearance(coupons_applied)
   return discounts_applied
 end
