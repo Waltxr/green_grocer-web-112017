009210c482d22369353268b0335298cb74ac3cd8
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 19:28:11 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -37,21 +37,18 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    minimum = coupon[:num]
-      while cart[coupon[:item]][:count] > coupon[:num]
-        if cart["#{coupon[:item]} W/COUPON"]
-          cart["#{coupon[:item]} W/COUPON"][:count] +=1
-        elsif cart[coupon[:item]]
-          cart["#{coupon[:item]} W/COUPON"] = {
-            :price=>coupon[:cost],
-            :clearance=>cart[coupon[:item]][:clearance],
-            :count => 1
-          }
-        else
-          return cart
-        end
-        cart[coupon[:item]][:count] -= coupon[:num]
+      if cart["#{coupon[:item]} W/COUPON"]
+        cart["#{coupon[:item]} W/COUPON"][:count] +=1
+      elsif cart[coupon[:item]]
+        cart["#{coupon[:item]} W/COUPON"] = {
+          :price=>coupon[:cost],
+          :clearance=>cart[coupon[:item]][:clearance],
+          :count => 1
+        }
+      else
+        return cart
       end
+      cart[coupon[:item]][:count] -= coupon[:num]
   end
   return cart
 end
