635333575831dd02a0b62dd3bdc6cec6b3208819
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 17:23:44 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -13,6 +13,7 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  puts coupons
   coupons.each do |coupon|
     cart["#{coupon[:item]} W/COUPON"] = {
       :price=>coupon[:cost],
