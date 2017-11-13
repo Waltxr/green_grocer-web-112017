5f9346849cd00588c16c44f264fe2c8c037c9e00
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 17:51:20 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -16,13 +16,13 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     if cart.keys.include?("#{coupon[:item]} W/COUPON")
       cart["#{coupon[:item]} W/COUPON"][:count] +=1
-    else
-    cart["#{coupon[:item]} W/COUPON"] = {
-      :price=>coupon[:cost],
-      :clearance=>cart[coupon[:item]][:clearance],
-      :count => 1
-    }
-    end
+    else cart.keys.include?(coupon[:item])  
+      cart["#{coupon[:item]} W/COUPON"] = {
+        :price=>coupon[:cost],
+        :clearance=>cart[coupon[:item]][:clearance],
+        :count => 1
+      }
+    end 
     cart[coupon[:item]][:count] -= coupon[:num]
   end
 
